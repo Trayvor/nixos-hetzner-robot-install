df88d34a7dcb811b17c2069ae5a60ed8d085d2d5
Vladyslav Herasymenko <vladyslav.herasymenkoo@gmail.com>
2025-04-15 00:35:57 +0200

    asda

--- configuration.nix
+++ configuration.nix
@@ -18,16 +18,51 @@
   };
   services.openssh.enable = true;
 
+  services.code-server = {
+    enable = true;
+    package = pkgs.code-server;  
+    port = 8080;
+    extraConfig = ''
+      auth: password
+      password: "jg10GCWN49mp" 
+      disable-telemetry: true
+      bind-addr: 127.0.0.1:8080
+    '';
+  };
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."code.clownworldsec.com" = {
+      root = "/var/www";
+      listen = [ { addr = "0.0.0.0"; port = 80; } ];
+      locations."/".proxyPass = "http://127.0.0.1:8080";
+      locations."/".extraConfig = ''
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host $host;
+      '';
+      enableACME = true;
+      acme = {
+        email = "henry.freeman@upliftmedia.us";  
+        acceptTerms = true;
+      };
+    };
+  };
+
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
     pkgs.gitMinimal
     pkgs.htop
     pkgs.python314
     pkgs.nano
+    pkgs.git
+    pkgs.gh
   ];
 
   users.users.root.openssh.authorizedKeys.keys = [
-    # change this to your ssh key
+    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDsQ6srSVBrCW79sxolSgX+jTK/TIOTUxuzgRMadcrdidZoHEjNIx1KrgCLF02tDg24eC+63LHBik+Olp/efPk6ghYb7d/GRflrzuRE2mCgxrtbxICM1TxzRpUMWQ1RSfDLwR7vT4Qbz+EGhfWfPfqWt4cYERKD+i+5RiphSKxczATa4dyXJSxXjb88VhoUCUY9fsmIgMG2GoP2omkCDNtZmzvw9ppPHUUxf/6ZFoaPTerYC84dGduKZJ0e6MknvsiCTMBbfJ5UaLy3niHzXL+hy10mcaQzgCg7spfMOoblVLZutONJW9VwHth7IbkP4ACLKHG96UOeUILagU8VqF7XusnbHQU0O2OtdAzxnb15B4LYaYZM7aPYlU4m/yheBPcO5pVZFBBkuTo4ppmFWfCd1o6D+fWZlHyZ4MurKO+ZzwtIgbf1D6VcCNtnWALrbEcMXGvaJ7MHQywUzjApM+cFGDCWMQ4OgzOFmEW1KEItx96vzovVYNDnIRGlHmag+Ysv1Gx/s/WwKXJCN4p7cWcPP4cc3LbwTvFwwplH8HNhdHJ0cjFZsNCdItM/olZj44mvWlwZwWMXj1rsExqxmKhmBmrCgIoy/E4a8UtiLoZUW7UKQ5ezB4XV1zjRPEYNQIilB/rGRmS8OAH02T+o9TpwGgSDwIXCjk99zIangVgxoQ== vladyslav.herasymenkoo@gmail.com
     ""
   ];
 
