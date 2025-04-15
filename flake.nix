072293c0d75f54f93f4e946fb936858b64215771
Vladyslav Herasymenko <vladyslav.herasymenkoo@gmail.com>
2025-04-15 03:11:59 +0200

    added code server

--- flake.nix
+++ flake.nix
@@ -3,14 +3,12 @@
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
-  inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
 
   outputs =
     {
       nixpkgs,
       disko,
       nixos-facter-modules,
-      vscode-server,
       ...
     }:
     {
@@ -18,13 +16,9 @@
       # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
       nixosConfigurations.generic = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          vscode-server.nixosModules.default
-          ({ config, pkgs, ... }: {
-            services.vscode-server.enable = true;
-          })
+        modules = [   
           disko.nixosModules.disko
-          ./disk-config
+          ./disk-config.nix
           ./configuration.nix
           ./hardware-configuration.nix
         ];
